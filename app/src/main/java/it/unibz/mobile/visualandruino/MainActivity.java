f9e3569702099442bbb0bd9bd954149f600fc2ea
Gand Fabian (Student Com17) <fgand@unibz.it>
2018-10-20 12:01:18 +0200

    added sketch + app with sample functionality

--- app/src/main/java/it/unibz/mobile/visualandruino/MainActivity.java
+++ app/src/main/java/it/unibz/mobile/visualandruino/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.List;
+
+import io.palaima.smoothbluetooth.Device;
+import io.palaima.smoothbluetooth.SmoothBluetooth;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 
 public class MainActivity extends Activity implements View.OnTouchListener {
@@ -16,20 +25,129 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     ViewGroup _root;
     private int _xDelta;
     private int _yDelta;
+    private SmoothBluetooth mSmoothBluetooth;
+
+    private SmoothBluetooth.Listener mListener = new SmoothBluetooth.Listener() {
+        @Override
+        public void onBluetoothNotSupported() {
+            //device does not support bluetooth
+        }
+
+        @Override
+        public void onBluetoothNotEnabled() {
+            //bluetooth is disabled, probably call Intent request to enable bluetooth
+        }
+
+        @Override
+        public void onConnecting(Device device) {
+            //called when connecting to particular device
+            //Toast.makeText(MainActivity.this, "Connecting to: " + device.getName(), Toast.LENGTH_SHORT).show();
+            //writeCommand("Connecting to: " + device.getName());
+        }
+
+        @Override
+        public void onConnected(Device device) {
+            //called when connected to particular device
+            //Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+            //writeCommand("Connected");
+
+        }
+
+        @Override
+        public void onDisconnected() {
+            //called when disconnected from device
+            //writeCommand("Disconnected");
+            mSmoothBluetooth.tryConnection();
+        }
+
+        @Override
+        public void onConnectionFailed(Device device) {
+            //called when connection failed to particular device
+            mSmoothBluetooth.tryConnection();
+        }
+
+        @Override
+        public void onDiscoveryStarted() {
+            //called when discovery is started
+        }
+
+        @Override
+        public void onDiscoveryFinished() {
+            //called when discovery is finished
+        }
+
+        @Override
+        public void onNoDevicesFound() {
+            //called when no devices found
+            mSmoothBluetooth.tryConnection();
+        }
+
+        @Override
+        public void onDevicesFound(final List<Device> deviceList,
+                                   final SmoothBluetooth.ConnectionCallback connectionCallback) {
+            //receives discovered devices list and connection callback
+            //you can filter devices list and connect to specific one
+            //connectionCallback.connectTo(deviceList.get(position));
+
+            Device spiderDevice = null;
+
+            for (Device device: deviceList) {
+
+                if(device.getName().equals("HC-05")) {
+                    spiderDevice = device;
+                }
+            }
+
+            connectionCallback.connectTo(spiderDevice);
+
+
+
+        }
+
+        @Override
+        public void onDataReceived(int data) {
+            //receives all bytes
+            /*mBuffer.add(data);
+            StringBuilder sb = new StringBuilder();
+
+            if(data == 6) {
+
+            }
+
+            if (data == 3 && !mBuffer.isEmpty()) {
+                //if (data == 0x0D && !mBuffer.isEmpty() && mBuffer.get(mBuffer.size()-2) == 0xA0) {
+
+                for (int integer : mBuffer) {
+                    sb.append((char) integer);
+                }
+                mBuffer.clear();
+
+                writeCommand(sb.toString());
+
+
+            }*/
+
+        }
+    };
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mSmoothBluetooth = new SmoothBluetooth(getApplicationContext());
+        mSmoothBluetooth.setListener(mListener);
+        mSmoothBluetooth.tryConnection();
+
+
         setContentView(R.layout.activity_main);
+
 
         _root = (ViewGroup)findViewById(R.id.root);
 
         ImageView  _view = (ImageView) findViewById(R.id.rectimage);
-        //_view = new ImageView(this);
-        //_view.setBackgroundResource(R.drawable.shape);
 
-
-        //_view.setText("TextView!!!!!!!!");
 
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(400, 200);
         layoutParams.leftMargin = 50;
@@ -39,7 +157,21 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         _view.setLayoutParams(layoutParams);
 
         _view.setOnTouchListener(this);
-        //_root.addView(_view);
+
+        final Button button = findViewById(R.id.codifyButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                EditText edit = (EditText)findViewById(R.id.pinNumber);
+                String pinNumber = edit.getText().toString();
+
+                String testCommand = "3 " + pinNumber + " 1;";
+
+                // Code here executes on main thread after user presses button
+                mSmoothBluetooth.send(testCommand, false);
+            }
+        });
+
     }
 
     public boolean onTouch(View view, MotionEvent event) {
