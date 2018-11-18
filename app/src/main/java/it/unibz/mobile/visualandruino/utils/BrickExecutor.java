af8129d40ea0a864256cce40700a5f11b4088f06
Gand Fabian (Student Com17) <fgand@unibz.it>
2018-11-18 19:49:29 +0100

    first draft: internal commands execution

--- app/src/main/java/it/unibz/mobile/visualandruino/utils/BrickExecutor.java
+++ app/src/main/java/it/unibz/mobile/visualandruino/utils/BrickExecutor.java
@@ -7,6 +7,8 @@ import it.unibz.mobile.visualandruino.models.Brick;
 import it.unibz.mobile.visualandruino.models.enums.BrickTypes;
 import it.unibz.mobile.visualandruino.models.InternalBrick;
 import it.unibz.mobile.visualandruino.models.Parameter;
+import it.unibz.mobile.visualandruino.models.enums.ComparatorTypes;
+import it.unibz.mobile.visualandruino.models.enums.InternalSubTypes;
 
 
 //Builder Class
@@ -29,7 +31,13 @@ public class BrickExecutor {
 
         } else if(currentBrick.getBrickType() == BrickTypes.INTERNAL) {
             // Execute subBricks recursively
-            executeBlocks(((InternalBrick) currentBrick).getSubBricks() , pinNumber);
+            if(((InternalBrick) currentBrick).getSubType() == InternalSubTypes.FOR) {
+                int forLoopLimiter = Integer.parseInt(currentBrick.getParameters().get(0).getValue());
+                for(int i=0; i<=forLoopLimiter; i++) {
+                    executeBlocks(((InternalBrick) currentBrick).getSubBricks() , pinNumber);
+                }
+            }
+
 
             // TODO Execute
         } else if(currentBrick.getBrickType() == BrickTypes.ANDROID) {
@@ -48,6 +56,105 @@ public class BrickExecutor {
         if(bricks.size()>0)
         {
             executeBlocks(bricks, pinNumber);
+        }
+
+
+    }
+
+    public void executeInternal(InternalBrick currentBrick, String pinNumber) {
+        /**
+         * *** FOR ***
+         */
+        if(currentBrick.getSubType() == InternalSubTypes.FOR) {
+            int referenceValue = Integer.parseInt(currentBrick.getParameters().get(0).getValue());
+            ComparatorTypes comparator = ComparatorTypes.valueOf(currentBrick.getParameters().get(1).getValue());
+            int forLoopLimiter = Integer.parseInt(currentBrick.getParameters().get(2).getValue());
+
+            if(comparator == ComparatorTypes.GREATER) {
+                for(int i = referenceValue; i<forLoopLimiter; i++) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                }
+            }
+
+            if(comparator == ComparatorTypes.LESS) {
+                for(int i= referenceValue; i>forLoopLimiter; i++) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                }
+            }
+        }
+
+        /**
+         * *** WHILE ***
+         */
+        if(currentBrick.getSubType() == InternalSubTypes.FOR) {
+            int referenceValue = Integer.parseInt(currentBrick.getParameters().get(0).getValue());
+            ComparatorTypes comparator = ComparatorTypes.valueOf(currentBrick.getParameters().get(1).getValue());
+            int loopLimiter = Integer.parseInt(currentBrick.getParameters().get(2).getValue());
+
+            if(comparator == ComparatorTypes.GREATER) {
+                while (referenceValue > loopLimiter) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                    referenceValue++;
+                }
+            }
+
+            if(comparator == ComparatorTypes.LESS) {
+                while (referenceValue < loopLimiter) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                    referenceValue++;
+                }
+            }
+
+            if(comparator == ComparatorTypes.EQUALS) {
+                while (referenceValue == loopLimiter) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                    referenceValue++;
+                }
+            }
+
+            if(comparator == ComparatorTypes.NOTEQUALS) {
+                while (referenceValue != loopLimiter) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                    referenceValue++;
+                }
+            }
+        }
+
+
+        /**
+         * *** IF ***
+         */
+        if(currentBrick.getSubType() == InternalSubTypes.IF) {
+            int referenceValue = Integer.parseInt(currentBrick.getParameters().get(0).getValue());
+            ComparatorTypes comparator = ComparatorTypes.valueOf(currentBrick.getParameters().get(1).getValue());
+            int secondValue = Integer.parseInt(currentBrick.getParameters().get(2).getValue());
+
+            if(comparator == ComparatorTypes.GREATER) {
+                if(referenceValue > secondValue) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                }
+            }
+
+            if(comparator == ComparatorTypes.LESS) {
+                if(referenceValue < secondValue) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                }
+            }
+
+            if(comparator == ComparatorTypes.EQUALS) {
+                if(referenceValue == secondValue) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                }
+            }
+
+            if(comparator != ComparatorTypes.EQUALS) {
+                if(referenceValue != secondValue) {
+                    executeBlocks(currentBrick.getSubBricks() , pinNumber);
+                }
+            }
+
+
+            //TODO more of them
         }
 
 
